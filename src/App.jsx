34bd2f571e1b21e9273794a86017b62418e751b4
smpKlaa <96190615+smpKlaa@users.noreply.github.com>
2025-11-13 11:27:26 +0200

    rework css, add PropType validation to MediaRow.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import './CSS/App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <h1>My App</h1>
+      <Home />
     </>
   );
 }
---
+++ src/components/MediaRow.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+const MediaRow = (props) => {
+  const {item} = props;
+
+  return (
+    <>
+      <tr key={item.media_id}>
+        <td>
+          <img src={item.thumbnail} alt={item.title}></img>
+        </td>
+        <td>{item.title}</td>
+        <td>{item.description}</td>
+        <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+        <td>{item.filesize}</td>
+        <td>{item.media_type}</td>
+      </tr>
+    </>
+  );
+};
+
+// PropType validation for item props.
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    filename: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    filesize: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
+    created_at: PropTypes.instanceOf(Date).isRequired,
+  }),
+};
+
+export default MediaRow;
